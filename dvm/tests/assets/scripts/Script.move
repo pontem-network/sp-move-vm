ea8b34467cc500847075468d9602e7e675994994
Dm. Yakushev <rig410@yandex.ru>
2020-12-24 17:04:32 +0300

    Added event handler test.

--- dvm/tests/assets/scripts/Script.move
+++ dvm/tests/assets/scripts/Script.move
@@ -1,7 +1,7 @@
 script {
     use 0x1::Store;
 
-    fun main(account: &signer, val: u64) {
+    fun store_u64(account: &signer, val: u64) {
         Store::store_u64(account, val);
     }
 }
--- dvm/tests/vm.rs
+++ dvm/tests/vm.rs
@@ -63,14 +63,38 @@ fn gas() -> Gas {
 
 fn store_module() -> ModuleTx {
     ModuleTx::new(
-        include_bytes!("assets/target/modules/0_Store.mv").to_vec(),
+        include_bytes!("assets/target/modules/1_Store.mv").to_vec(),
         CORE_CODE_ADDRESS,
     )
 }
 
-fn script(args: u64) -> ScriptTx {
+fn event_module() -> ModuleTx {
+    ModuleTx::new(
+        include_bytes!("assets/target/modules/0_Event.mv").to_vec(),
+        CORE_CODE_ADDRESS,
+    )
+}
+
+fn vector_module() -> ModuleTx {
+    ModuleTx::new(
+        include_bytes!("assets/target/modules/2_Vector.mv").to_vec(),
+        CORE_CODE_ADDRESS,
+    )
+}
+
+fn store_script(args: u64) -> ScriptTx {
     ScriptTx::new(
-        include_bytes!("assets/target/scripts/0_main.mv").to_vec(),
+        include_bytes!("assets/target/scripts/1_store_u64.mv").to_vec(),
+        vec![Value::u64(args)],
+        vec![],
+        vec![CORE_CODE_ADDRESS],
+    )
+    .unwrap()
+}
+
+fn emit_event_script(args: u64) -> ScriptTx {
+    ScriptTx::new(
+        include_bytes!("assets/target/scripts/0_emit_event.mv").to_vec(),
         vec![Value::u64(args)],
         vec![],
         vec![CORE_CODE_ADDRESS],
@@ -92,7 +116,7 @@ fn test_public_module() {
     let store_module_id = ModuleId::new(CORE_CODE_ADDRESS, Identifier::new("Store").unwrap());
     assert_eq!(
         state.get_module(&store_module_id).unwrap().unwrap(),
-        include_bytes!("assets/target/modules/0_Store.mv").to_vec()
+        include_bytes!("assets/target/modules/1_Store.mv").to_vec()
     );
 }
 
@@ -104,7 +128,7 @@ fn test_execute_script() {
     let vm = Dvm::new(mock.clone(), event_handler.clone());
     let state = State::new(mock.clone());
     vm.publish_module(gas(), store_module()).unwrap();
-    vm.execute_script(gas(), script(test_value)).unwrap();
+    vm.execute_script(gas(), store_script(test_value)).unwrap();
 
     let tag = StructTag {
         address: CORE_CODE_ADDRESS,
@@ -118,4 +142,30 @@ fn test_execute_script() {
         .unwrap();
     let store: StoreU64 = lcs::from_bytes(&blob).unwrap();
     assert_eq!(test_value, store.val);
+}
+
+#[test]
+fn test_store_event() {
+    let test_value = 13;
+    let mock = StorageMock::new();
+    let event_handler = EventHandlerMock::default();
+    let vm = Dvm::new(mock, event_handler.clone());
+    vm.publish_module(gas(), vector_module()).unwrap();
+    vm.publish_module(gas(), event_module()).unwrap();
+    vm.execute_script(gas(), emit_event_script(test_value))
+        .unwrap();
+
+    let (guid, seq, tag, msg) = event_handler.data.borrow_mut().remove(0);
+    assert_eq!(guid, b"GUID".to_vec());
+    assert_eq!(seq, 1);
+    assert_eq!(test_value, lcs::from_bytes::<StoreU64>(&msg).unwrap().val);
+    assert_eq!(
+        TypeTag::Struct(StructTag {
+            address: CORE_CODE_ADDRESS,
+            module: Identifier::new("Event").unwrap(),
+            name: Identifier::new("U64").unwrap(),
+            type_params: vec![],
+        }),
+        tag
+    );
 }
