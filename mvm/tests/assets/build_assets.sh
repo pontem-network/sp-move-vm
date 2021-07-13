dc403bc82e36a074f08c9cb3e585fd91e4cce40f
Vladimir Serov <me@cab404.ru>
2021-07-13 18:55:57 +0300

    Added dove version check to build_assets.sh

--- mvm/tests/assets/build_assets.sh
+++ mvm/tests/assets/build_assets.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+set -e
+ver=$(dove -V | sed 's|\.|000|g;s|Dove ||')
+if [ 100020002 -gt $ver ]; then
+    echo "Your Dove is too old. Please, update at least to version 1.2.2."
+    exit 1
+fi
+
 dove build -u
 dove tx "store_u64(13)"
 dove tx "tx_test<0x01::Pontem::T>(100)"
