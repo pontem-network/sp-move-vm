7ef6b47ad76da78884abae6389da7ccbec4c32f1
rig410 <rig410@yandex.ru>
2021-12-02 17:01:23 +0300

    CI wip.

--- mvm/tests/assets/build_assets.sh
+++ mvm/tests/assets/build_assets.sh
@@ -1,4 +1,32 @@
 #!/usr/bin/env bash
+
+ function lockfile_waithold()
+ {
+    declare -ir time_beg=$(date '+%s')
+    declare -ir time_max=7140  # 7140 s = 1 hour 59 min.
+
+    while ! \
+       (set -o noclobber ; \
+        echo -e "DATE:$(date)\nUSER:$(whoami)\nPID:$$" > /tmp/global.lock \
+       ) 2>/dev/null
+    do
+        if [ $(($(date '+%s') - ${time_beg})) -gt ${time_max} ] ; then
+            echo "Error: waited too long for lock file /tmp/global.lock" 1>&2
+            return 1
+        fi
+        sleep 1
+    done
+
+    return 0
+ }
+
+ function lockfile_release()
+ {
+    rm -f /tmp/global.lock
+ }
+
+lockfile_waithold
+
 set -e
 dove clean --global
 
@@ -13,3 +41,6 @@ dove tx "tr_signers(tr)"
 dove tx "tr_and_rt_signers(root, treasury)"
 dove tx "signers_tr_and_rt_with_user(root, treasury)"
 dove tx "0x1::ScriptBook::test"
+
+lockfile_release
+
